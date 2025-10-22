d038eee1938ad61698eb758e7aa272af4909b87d
mofoyikes <mofeticyikerson1024@gmail.com>
2025-10-22 13:10:14 -0400

    create turret subsystem

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -4,12 +4,24 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TurretSubsystem extends SubsystemBase {
   /** Creates a new TurretSubsystem. */
+
+  private SparkMax rotationMotor;
+  private SparkFlex leftPivotMotor;
+  private SparkFlex rightPivotMotor;
   
-  public TurretSubsystem() {}
+  public TurretSubsystem() {
+    rotationMotor = new SparkMax(11, MotorType.kBrushless);
+    leftPivotMotor = new SparkFlex(12, MotorType.kBrushless);
+    rightPivotMotor = new SparkFlex(13, MotorType.kBrushless);
+  }
 
   @Override
   public void periodic() {
